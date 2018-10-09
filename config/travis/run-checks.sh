cd4921da1b7a0dcdde1113a068cbfe66fc3ca2f4
ShreyasKp <shreyaskp1999@gmail.com>
2018-10-09 23:46:44 +0800

    Add Travis test cases to test branch

---
+++ config/travis/run-checks.sh
@@ -0,0 +1,12 @@
+# Solution below adapted from addressbook-level4 by GitHub user pyokagan
+#!/bin/sh
+# Runs all check-* scripts, and returns a non-zero exit code if any of them fail.
+
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd) &&
+ret=0 &&
+for checkscript in "$dir"/check-*; do
+    if ! "$checkscript"; then
+        ret=1
+    fi
+done
+exit $ret
