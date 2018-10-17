06bef0ade557bbb1775755b168374249d4c7cc25
Putra Danish <iamputradanish@gmail.com>
2018-10-17 15:54:58 +0800

    fix exception bug

--- src/seedu/addressbook/readandwrite/ReaderAndWriter.java
+++ src/seedu/addressbook/readandwrite/ReaderAndWriter.java
@@ -1,27 +1,4 @@
 package seedu.addressbook.readandwrite;
 
-import java.io.*;
-
 public class ReaderAndWriter {
-
-    public File fileToUse (String pathName){
-        File file = new File(pathName);
-        return file;
-    }
-
-    public BufferedReader openReader (File file) throws FileNotFoundException {
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        return br;
-    }
-
-    public PrintWriter openWriter (File file) throws IOException {
-        PrintWriter pw = new PrintWriter(file);
-        return pw;
-    }
-
-    public PrintWriter openTempWriter (File file) throws IOException {
-        PrintWriter pw = new PrintWriter(new FileWriter(file));
-        return pw;
-    }
-
 }
