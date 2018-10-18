b0d2c7e3a731f75633334c492a181afc552aa396
Ong Wee Keong <ongweekeong@gmail.com>
2018-10-19 01:49:24 +0800

    Inbox Command 'showunread' working - displays list of unread messages.

--- src/seedu/addressbook/commands/InboxCommand.java
+++ src/seedu/addressbook/commands/InboxCommand.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.TreeSet;
 
 public class InboxCommand extends Command {
-    public static final String COMMAND_WORD = "unread";
+    public static final String COMMAND_WORD = "showunread";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
             + "Displays all unread messages in the application starting from the most urgent.\n\t"
@@ -21,11 +21,21 @@ public class InboxCommand extends Command {
         Inbox myInbox = new Inbox();
         TreeSet<Msg> allMsgs = null; // TODO: Figure out how to send the whole list of messages over.
         int myUnreadMsgs;
+        int messageNum = 1;
+        Msg msgToPrint;
         try {
             allMsgs = myInbox.loadMsgs();
             myUnreadMsgs = myInbox.checkNumUnreadMessages();
             if(myUnreadMsgs!=0) {
-                return new CommandResult(String.format(Messages.MESSAGE_UNREAD_MSG_NOTIFICATION, myUnreadMsgs));
+                String fullPrintedMessage = Messages.MESSAGE_UNREAD_MSG_NOTIFICATION + '\n';
+                for(int i=0; i<myUnreadMsgs; i++){
+                    msgToPrint = allMsgs.pollFirst();
+                    assert msgToPrint != null;
+                    fullPrintedMessage += String.valueOf(messageNum++) + ".\t[UNREAD] Priority: " + msgToPrint.getPriority() + ", Sent: " +
+                            msgToPrint.getTime() + ", message: " + msgToPrint.getMsg() + ", Coordinates: " + msgToPrint.getLatitude() + ", " +
+                            msgToPrint.getLongitude() + ", ETA: " + msgToPrint.getEta() + ".\n";
+                }
+                return new CommandResult(String.format(fullPrintedMessage, myUnreadMsgs));
             }
             else{
                 return new CommandResult(Messages.MESSAGE_NO_UNREAD_MSGS);
