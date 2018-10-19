bb686209baf2505e88f1d8c9a3ab2251a0b8dcce
Matthew Alexander <alexandermatthew12@gmail.com>
2018-10-19 12:59:51 +0800

    Fixed missing person edit command bug

--- src/seedu/addressbook/common/Messages.java
+++ src/seedu/addressbook/common/Messages.java
@@ -10,8 +10,8 @@ public class Messages {
     public static final String MESSAGE_PERSON_NOT_IN_ADDRESSBOOK = "Person could not be found in system";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_TIMESTAMPS_LISTED_OVERVIEW = "%1$d time(s) screened";
-    public static final String MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE = "Launch command format: " +
-            "java seedu.addressbook.Main [STORAGE_FILE_PATH]";
+//    public static final String MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE = "Launch command format: " +
+//            "java seedu.addressbook.Main [STORAGE_FILE_PATH]";
     public static final String MESSAGE_WELCOME = "Welcome to your system.";
     public static final String MESSAGE_USING_STORAGE_FILE = "Using storage file : %1$s";
     public static final String MESSAGE_ENTER_PASSWORD = "Please enter password. ";
