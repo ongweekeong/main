a5d12e06fe521c7d2f60177e736a978313b2030d
Matthew Alexander <alexandermatthew12@gmail.com>
2018-11-01 19:57:00 +0800

    Refactored police officer ids

--- src/seedu/addressbook/PatrolResourceStatus.java
+++ src/seedu/addressbook/PatrolResourceStatus.java
@@ -2,7 +2,7 @@
 package seedu.addressbook;
 
 import org.javatuples.Triplet;
-import seedu.addressbook.commands.UpdateStatusCommand;
+
 import seedu.addressbook.common.Location;
 import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.exception.IllegalValueException;
@@ -10,14 +10,21 @@ import seedu.addressbook.data.exception.IllegalValueException;
 import java.util.ArrayList;
 
 public class PatrolResourceStatus {
+    public static final String HEADQUARTER_PERSONNEL_ID = "hqp";
+    public static final String POLICE_OFFICER_1_ID = "po1";
+    public static final String POLICE_OFFICER_2_ID = "po2";
+    public static final String POLICE_OFFICER_3_ID = "po3";
+    public static final String POLICE_OFFICER_4_ID = "po4";
+    public static final String POLICE_OFFICER_5_ID = "po5";
+
     // Triplet<Police Officer ID, Location, isEngaged
     private static ArrayList<Triplet<String, Location, Boolean>> patrolResourceStatus = new ArrayList<>(){{
-        add( new Triplet<>("hqp", new Location(1.294166, 103.770730), true) ); // NUS FASS
-        add( new Triplet<>("po1", new Location(1.306935, 103.790564), false) ); // Buona Vista
-        add( new Triplet<>("po2", new Location(1.346301, 103.682060), false) ); // NTU
-        add( new Triplet<>("po3", new Location(1.296057, 103.849865), false) ); // SMU
-        add( new Triplet<>("po4", new Location(1.340352, 103.962193), false) ); // SUTD
-        add( new Triplet<>("po5", new Location(1.329393, 103.776169), false) ); // SIM
+        add( new Triplet<>(HEADQUARTER_PERSONNEL_ID, new Location(1.294166, 103.770730), true) ); // NUS FASS
+        add( new Triplet<>(POLICE_OFFICER_1_ID, new Location(1.306935, 103.790564), false) ); // Buona Vista
+        add( new Triplet<>(POLICE_OFFICER_2_ID, new Location(1.346301, 103.682060), false) ); // NTU
+        add( new Triplet<>(POLICE_OFFICER_3_ID, new Location(1.296057, 103.849865), false) ); // SMU
+        add( new Triplet<>(POLICE_OFFICER_4_ID, new Location(1.340352, 103.962193), false) ); // SUTD
+        add( new Triplet<>(POLICE_OFFICER_5_ID, new Location(1.329393, 103.776169), false) ); // SIM
     }};
 
     public static ArrayList<Triplet<String, Location, Boolean>> getPatrolResourceStatus() {
@@ -27,7 +34,7 @@ public class PatrolResourceStatus {
     public static void resetPatrolResourceStatus() {
         int index = 0;
         for (Triplet<String, Location, Boolean> policeOfficer : patrolResourceStatus) {
-            if (policeOfficer.getValue0().equalsIgnoreCase("hqp")) {
+            if (policeOfficer.getValue0().equalsIgnoreCase(HEADQUARTER_PERSONNEL_ID)) {
                 patrolResourceStatus.set(index, Triplet.with(policeOfficer.getValue0(),policeOfficer.getValue1(), true));
             } else {
                 patrolResourceStatus.set(index, Triplet.with(policeOfficer.getValue0(),policeOfficer.getValue1(), false));
@@ -38,24 +45,24 @@ public class PatrolResourceStatus {
 
     public static Triplet<String, Location, Boolean> getPatrolResource(String patrolResource) {
         switch (patrolResource) {
-            case "hqp":
+            case HEADQUARTER_PERSONNEL_ID:
                 return patrolResourceStatus.get(0);
-            case "po1":
+            case POLICE_OFFICER_1_ID:
                 return patrolResourceStatus.get(1);
-            case "po2":
+            case POLICE_OFFICER_2_ID:
                 return patrolResourceStatus.get(2);
-            case "po3":
+            case POLICE_OFFICER_3_ID:
                 return patrolResourceStatus.get(3);
-            case "po4":
+            case POLICE_OFFICER_4_ID:
                 return patrolResourceStatus.get(4);
-            case "po5":
+            case POLICE_OFFICER_5_ID:
                 return patrolResourceStatus.get(5);
             default:
                 return patrolResourceStatus.get(0); // default for test cases
         }
     }
 
-    // TODO: put into message
+    // TODO: remove this duplicate code
     public static Location getLocation(String patrolResource) {
         switch (patrolResource) {
             case "hqp":
