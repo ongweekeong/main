a7de8130ee1a6a33e97c72683ca02b1b6721a5ef
ShreyasKp <shreyaskp1999@gmail.com>
2018-11-01 17:56:51 +0800

    Add test for Autocorrection feature for command add

--- test/java/seedu/addressbook/logic/LogicTest.java
+++ test/java/seedu/addressbook/logic/LogicTest.java
@@ -6,7 +6,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import seedu.addressbook.PatrolResourceStatus;
+import seedu.addressbook.autocorrect.AutoCorrect;
+import seedu.addressbook.autocorrect.CheckDistance;
 import seedu.addressbook.commands.*;
+import seedu.addressbook.commands.Dictionary;
 import seedu.addressbook.common.HttpRestClient;
 import seedu.addressbook.common.Location;
 import seedu.addressbook.common.Messages;
@@ -1352,5 +1355,26 @@ public class LogicTest {
     }
 
     //@@author ShreyasKp
-    //private void
+    /**
+     * Asserts that running the AutoCorrection algorithm on the given inputs will return prediction with the given feedback message.
+     */
+    @Test
+    public void execute_addCommand_wrongSpellingOfCommandWord() {
+        CheckDistance checker = new CheckDistance();
+        AutoCorrect correction = new AutoCorrect();
+        Dictionary dict = new Dictionary();
+        final String[] inputs = {
+                "ad",
+                "ade",
+                "adds"
+        };
+        String expected = new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE)).feedbackToUser;
+        expected = expected.substring(expected.indexOf("!") + 1);
+        for (String input: inputs) {
+            String output = checker.checkDistance(input);
+            String suggestion = String.format(dict.getCommandErrorMessage(), output);
+            String displayCommand = correction.checkCommand(input);
+            assertEquals(String.format(dict.errorCommandMessage, output) + "\n" + expected,suggestion+"\n"+displayCommand);
+        }
+    }
 }
