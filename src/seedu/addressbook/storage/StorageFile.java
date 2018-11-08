efcc7322613de811e05b646ed096a28f29be026f
Putra Danish <iamputradanish@gmail.com>
2018-11-09 01:44:40 +0800

    Populated police records with 20 contacts

--- src/seedu/addressbook/storage/StorageFile.java
+++ src/seedu/addressbook/storage/StorageFile.java
@@ -133,6 +133,7 @@ public class StorageFile {
             save(populated);
             return populated;
 
+
         // other errors
         } catch (IOException ioe) {
             throw new StorageOperationException("Error writing to file: " + path);
@@ -148,23 +149,168 @@ public class StorageFile {
     }
 
     //@@iamputradanish
-    public AddressBook populatedPoliceRecords() throws IllegalValueException {
+    private AddressBook populatedPoliceRecords() throws IllegalValueException {
         return new AddressBook(new UniquePersonList(
                 new Person(
-                        new Name("John Doe"),
-                        new NRIC("s1234567a"),
-                        new DateOfBirth("1996"),
+                        new Name("AJ Styles"),
+                        new NRIC("s8012345a"),
+                        new DateOfBirth("1980"),
                         new PostalCode("510246"),
                         new Status("xc"),
                         new Offense("none"),
-                        Collections.singleton(new Offense("riot"))),
+                        Set.of(new Offense("theft"), new Offense("assault"))),
                 new Person(
-                        new Name("Jane Doe"),
+                        new Name("Becky Lynch"),
                         new NRIC("s9611234c"),
-                        new DateOfBirth("1997"),
-                        new PostalCode("510246"),
+                        new DateOfBirth("1996"),
+                        new PostalCode("948372"),
                         new Status("xc"),
                         new Offense("none"),
-                        Collections.singleton(new Offense("riot")))));
+                        Set.of(new Offense("riot"))),
+                new Person(
+                        new Name("Brock Lesnar"),
+                        new NRIC("s7512345a"),
+                        new DateOfBirth("1975"),
+                        new PostalCode("213456"),
+                        new Status("clear"),
+                        new Offense("none"),
+                        Set.of(new Offense("none"))),
+                new Person(
+                        new Name("Cesaro"),
+                        new NRIC("s8812341g"),
+                        new DateOfBirth("1988"),
+                        new PostalCode("402917"),
+                        new Status("wanted"),
+                        new Offense("murder"),
+                        Set.of(new Offense("kidnap"))),
+                new Person(
+                        new Name("Pete Dunne"),
+                        new NRIC("s9443567h"),
+                        new DateOfBirth("1994"),
+                        new PostalCode("234567"),
+                        new Status("xc"),
+                        new Offense("none"),
+                        Set.of(new Offense("piracy"), new Offense("assault"))),
+                new Person(
+                        new Name("Ronda Rousey"),
+                        new NRIC("s9154362c"),
+                        new DateOfBirth("1991"),
+                        new PostalCode("567342"),
+                        new Status("clear"),
+                        new Offense("none"),
+                        Set.of(new Offense("none"))),
+                new Person(
+                        new Name("Sheamus"),
+                        new NRIC("s8512356e"),
+                        new DateOfBirth("1985"),
+                        new PostalCode("908371"),
+                        new Status("clear"),
+                        new Offense("none"),
+                        Set.of(new Offense("none"))),
+                new Person(
+                        new Name("Seth Rollins"),
+                        new NRIC("s9012345k"),
+                        new DateOfBirth("1990"),
+                        new PostalCode("975241"),
+                        new Status("wanted"),
+                        new Offense("assault"),
+                        Set.of(new Offense("riot"))),
+                new Person(
+                        new Name("Shinsuke Nakamura"),
+                        new NRIC("s8412345c"),
+                        new DateOfBirth("1984"),
+                        new PostalCode("738492"),
+                        new Status("xc"),
+                        new Offense("none"),
+                        Set.of(new Offense("cheating"), new Offense("theft"))),
+                new Person(
+                        new Name("Adam Cole"),
+                        new NRIC("s9677843c"),
+                        new DateOfBirth("1996"),
+                        new PostalCode("545246"),
+                        new Status("clear"),
+                        new Offense("none"),
+                        Set.of(new Offense("none"))),
+                new Person(
+                        new Name("Aleister Black"),
+                        new NRIC("s9212386d"),
+                        new DateOfBirth("1992"),
+                        new PostalCode("125773"),
+                        new Status("xc"),
+                        new Offense("none"),
+                        Set.of(new Offense("robbery"), new Offense("rape"))),
+                new Person(
+                        new Name("Alexa Bliss"),
+                        new NRIC("s9898325j"),
+                        new DateOfBirth("1998"),
+                        new PostalCode("876524"),
+                        new Status("wanted"),
+                        new Offense("rape"),
+                        Set.of(new Offense("none"))),
+                new Person(
+                        new Name("Apollo Crews"),
+                        new NRIC("s8988374e"),
+                        new DateOfBirth("1989"),
+                        new PostalCode("214563"),
+                        new Status("xc"),
+                        new Offense("none"),
+                        Set.of(new Offense("riot"))),
+                new Person(
+                        new Name("Baron Corbin"),
+                        new NRIC("s9001235z"),
+                        new DateOfBirth("1990"),
+                        new PostalCode("468974"),
+                        new Status("wanted"),
+                        new Offense("murder"),
+                        Set.of(new Offense("robbery"), new Offense("theft"))),
+                new Person(
+                        new Name("Randy Orton"),
+                        new NRIC("s8298760a"),
+                        new DateOfBirth("1982"),
+                        new PostalCode("568986"),
+                        new Status("xc"),
+                        new Offense("none"),
+                        Set.of(new Offense("assault"))),
+                new Person(
+                        new Name("Paul Heyman"),
+                        new NRIC("s7656436d"),
+                        new DateOfBirth("1976"),
+                        new PostalCode("987764"),
+                        new Status("clear"),
+                        new Offense("none"),
+                        Set.of(new Offense("none"))),
+                new Person(
+                        new Name("Matt Hardy"),
+                        new NRIC("s8712392n"),
+                        new DateOfBirth("1987"),
+                        new PostalCode("928716"),
+                        new Status("xc"),
+                        new Offense("none"),
+                        Set.of(new Offense("theft"))),
+                new Person(
+                        new Name("Jeff Hardy"),
+                        new NRIC("s9023472c"),
+                        new DateOfBirth("1990"),
+                        new PostalCode("029837"),
+                        new Status("clear"),
+                        new Offense("none"),
+                        Set.of(new Offense("none"))),
+                new Person(
+                        new Name("John Cena"),
+                        new NRIC("s8017234a"),
+                        new DateOfBirth("1980"),
+                        new PostalCode("920175"),
+                        new Status("xc"),
+                        new Offense("none"),
+                        Set.of(new Offense("robbery"), new Offense("assault"))),
+                new Person(
+                        new Name("Kevin Owens"),
+                        new NRIC("s9128391f"),
+                        new DateOfBirth("1991"),
+                        new PostalCode("928615"),
+                        new Status("xc"),
+                        new Offense("none"),
+                        Set.of(new Offense("riot"), new Offense("theft")))
+                        ));
     }
 }
