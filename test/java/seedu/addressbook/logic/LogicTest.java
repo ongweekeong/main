67bd6f5936543834c988910874fea464a8e5f86a
Putra Danish <iamputradanish@gmail.com>
2018-11-12 21:46:37 +0800

    fix bug

--- test/java/seedu/addressbook/logic/LogicTest.java
+++ test/java/seedu/addressbook/logic/LogicTest.java
@@ -9,7 +9,27 @@ import static seedu.addressbook.common.Messages.MESSAGE_INBOX_FILE_NOT_FOUND;
 import static seedu.addressbook.common.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.addressbook.common.Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK;
 import static seedu.addressbook.common.Messages.MESSAGE_TIMESTAMPS_LISTED_OVERVIEW;
-import static seedu.addressbook.password.Password.*;
+import static seedu.addressbook.password.Password.MESSAGE_ENTER_COMMAND;
+import static seedu.addressbook.password.Password.MESSAGE_ENTER_PASSWORD;
+import static seedu.addressbook.password.Password.MESSAGE_HQP;
+import static seedu.addressbook.password.Password.MESSAGE_PASSWORD_LENGTH;
+import static seedu.addressbook.password.Password.MESSAGE_PASSWORD_MINIMUM_LENGTH;
+import static seedu.addressbook.password.Password.MESSAGE_TRY_AGAIN;
+import static seedu.addressbook.password.Password.MESSAGE_VALID;
+import static seedu.addressbook.password.Password.correctHqp;
+import static seedu.addressbook.password.Password.correctPO1;
+import static seedu.addressbook.password.Password.correctPO2;
+import static seedu.addressbook.password.Password.correctPO3;
+import static seedu.addressbook.password.Password.correctPO4;
+import static seedu.addressbook.password.Password.correctPO5;
+import static seedu.addressbook.password.Password.getFullId;
+import static seedu.addressbook.password.Password.getId;
+import static seedu.addressbook.password.Password.getIsUpdatingPassword;
+import static seedu.addressbook.password.Password.isUpdatePasswordConfirmNow;
+import static seedu.addressbook.password.Password.lockDevice;
+
+import static seedu.addressbook.password.Password.unlockHqp;
+import static seedu.addressbook.password.Password.unlockPo;
 
 import java.io.IOException;
 import java.sql.Timestamp;
